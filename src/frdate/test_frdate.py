6f3c11861682134863134ce12c3903e18f044027
Thibaut Spriet <78731966+ThbtSprt@users.noreply.github.com>
2021-06-25 11:29:02 +0200

    Update test_frdate.py

--- src/frdate/test_frdate.py
+++ src/frdate/test_frdate.py
@@ -1,10 +1,17 @@
 from frdate import frdate
 
+d='14 juillet 1789'
 echantillon=[
-  ('14071789',False,False,'14 juillet 1789'),
-  ('14/07/1789',False,False,'14 juillet 1789')
+  ('14071789',False,False),
+  ('14/07/1789',False,False),
+  ('17890714',False,False),
+  ('14 07 1789',False,False),
+  ('14 juillet 1789',False,False),
+  ('1789-07-14',False,False)
    ]
 
 def test_fr_conv():
+  assert frdate.conv('2000-01-01',litteral=True) == 'premier janvier deux mille'
+  assert frdate.conv('2000-01-01',True) == datetime.date(2000,1,1)
   for t in echantillon:
-    assert frdate.conv(t[0],t[1],t[2]) == t[3]
+    assert frdate.conv(t[0],t[1],t[2]) == d
