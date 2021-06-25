ee7f4a2df16ee40e3ce3d393a2ab9aa656e6dcc5
Thibaut Spriet <78731966+ThbtSprt@users.noreply.github.com>
2021-06-25 11:37:14 +0200

    Update test_frdate.py

--- src/frdate/test_frdate.py
+++ src/frdate/test_frdate.py
@@ -14,5 +14,6 @@ echantillon=[
 def test_fr_conv():
   assert frdate.conv('2000-01-01',litteral=True) == 'premier janvier deux mille'
   assert frdate.conv('2000-01-01',True) == datetime.date(2000,1,1)
+  assert frdate.conv('10101212') == '12 décembre 1010'
   for t in echantillon:
     assert frdate.conv(t[0],t[1],t[2]) == d
