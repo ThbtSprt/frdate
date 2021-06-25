9fa7da7e4ad841953e94daf8404c709cc1ef9601
Thibaut Spriet <78731966+ThbtSprt@users.noreply.github.com>
2021-06-25 11:24:31 +0200

    Update test_frdate.py

--- src/frdate/test_frdate.py
+++ src/frdate/test_frdate.py
@@ -1,9 +1,10 @@
 from frdate import frdate
 
 echantillon=[
-  ('14071789','14 juillet 1789')
+  ('14071789',False,False,'14 juillet 1789'),
+  ('14/07/1789',False,False,'14 juillet 1789')
    ]
 
 def test_fr_conv():
   for t in echantillon:
-    assert frdate.conv(t[0]) == t[1]
+    assert frdate.conv(t[0],t[1],t[2]) == t[3]
